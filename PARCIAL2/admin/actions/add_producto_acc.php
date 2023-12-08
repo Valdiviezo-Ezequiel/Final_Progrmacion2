432d83d6b41519e2bf909d586137f9f58ae363a0
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-12-08 17:56:16 -0300

    fix add producto

--- PARCIAL2/admin/actions/add_producto_acc.php
+++ PARCIAL2/admin/actions/add_producto_acc.php
@@ -3,65 +3,32 @@ require_once "../../functions/autoload.php";
 
 $postData = $_POST;
 
-try {
-
-    (new Producto())->insert(
-        $postData['nombre'],
-        $postData['descripcion'],
-        $postData['categorias_id'],
-        $postData['talles'],
-        $postData['color'],
-        $postData['genero_id'],
-        $postData['disciplinas_id'],
-        $postData['precio'],
-        $postData['fecha_lanzamiento'],
-        $postData['envio'],
-        $postData['imagen'],
-    );
-    (new Alerta())->add_alerta('primary', "Se cargo exitosamente el producto");
-    header('Location: ../index.php?sec=admin_producto');
-} catch (Exception $e) {
-    (new Alerta())->add_alerta('danger', "No se pudo cargar el producto");
-    header('Location: ../index.php?sec=admin_producto');
-}
-
-?>
-
-<!-- <?PHP
-require_once "../../functions/autoload.php";
-
-$postData = $_POST;
+// echo "<pre>";
+// print_r($postData);
+// echo "</pre>";
 
 try {
-    // Validar y obtener el valor de 'genero_id'
-    $genero_id = filter_input(INPUT_POST, 'genero_id', FILTER_VALIDATE_INT);
-
-    // Verificar si 'genero_id' es un valor válido
-    if ($genero_id === false) {
-        // Manejar el error, redirigir o mostrar un mensaje al usuario
-        echo "Error: El valor de genero_id no es válido.";
-        exit;
-    }
 
     (new Producto())->insert(
         $postData['nombre'],
         $postData['descripcion'],
         $postData['categoria_id'],
-        $postData['talles'],
-        $postData['color'],
-        $genero_id,
+        $postData['genero_id'],
         $postData['disciplinas_id'],
-        $postData['precio'],
+        $postData['talles'],
         $postData['fecha_lanzamiento'],
-        $postData['envio'],
+        $postData['color'],
         $postData['imagen'],
+        $postData['envio'],
+        $postData['precio'],
     );
     (new Alerta())->add_alerta('primary', "Se cargo exitosamente el producto");
     header('Location: ../index.php?sec=admin_producto');
 } catch (Exception $e) {
-    // echo "Error: " . $e->getMessage() . "Data" . $postData;
-    echo "Error: " . $e->getMessage() . " Data: " . print_r($postData, true);
     (new Alerta())->add_alerta('danger', "No se pudo cargar el producto");
-    //header('Location: ../index.php?sec=admin_producto');
-}  
-?>-->
+     header('Location: ../index.php?sec=admin_producto');
+}
+
+?>
+
+
